64611a1e282692857e552081ab1faa23bf06ac4b
vprusa <prusa.vojtech@gmail.com>
2022-10-29 20:02:32 -0400

    bkp

--- VisualizationBase3D.py
+++ VisualizationBase3D.py
@@ -18,6 +18,11 @@ import matplotlib.pyplot as plt
 # matplotlib.use("TkAgg")
 # matplotlib.use("QtAgg")
 
+class Point:
+    x = 0
+    y = 0
+    z = 0
+
 class VisualizationBase3D(object):
     frameNo = 0
 
@@ -30,24 +35,20 @@ class VisualizationBase3D(object):
     plt = None
     layout = None
     fig = None
+    ax = None
 
-    def __init__(s, nxgraphType=None, nxgraphOptions=None, graphData=None, isDirected=False, frameTimeout=1):
+    plane = [-32, 32, 60]
+    points_cnt = 50
+
+    data: Point = []
+    data_old: Point = []
+
+    def __init__(s):
         s.plt = plt
 
         s.fig = plt.figure()
-        # ax1 = plt.axes(projection='3d')
         ax = s.plt.axes(projection='3d')
-
-        # Data for a three-dimensional line
-        # zline = np.linspace(0, 15, 1000)
-        # xline = np.sin(zline)
-        # yline = np.cos(zline)
-        # ax.plot3D(xline, yline, zline, 'gray')
-
-        # def ackley(xi, yi, a, b, c, d):
-        #     part1 = - a * np.exp((-b * np.sqrt((1.0/d) * np.power(xi, 2))))
-        #     part2 = - np.exp((1.0/d) * np.cos(c*xi))
-        #     return part1 + part2 + a + np.exp(1)
+        s.ax = ax
 
         def ackley(xi, yi, a, b, c, d):
             part1 = - a * np.exp((-b * np.sqrt((1.0/d) * (np.power(xi, 2) + np.power(yi, 2)))))
@@ -56,54 +57,47 @@ class VisualizationBase3D(object):
 
         def f(xi, xi1):
             # Rosenbrock Function
-            return ( 100 * np.power((xi1 - np.power(xi, 2)),2) + np.power((xi - 1), 2))
+            return (100 * np.power((xi1 - np.power(xi, 2)),2) + np.power((xi - 1), 2))
 
-        x = np.linspace(-32, 32, 60)
-        y = np.linspace(-32, 32, 60)
+        x = np.linspace(s.plane[0], s.plane[1], s.plane[2])
+        y = np.linspace(s.plane[0], s.plane[1], s.plane[2])
 
-        # X, Y = np.meshgrid(x, y)
-        # Z = f(X, Y)
-        # Z = f(X, Y)
-        # Recommended variable values are: a = 20, b = 0.2 and c = 2π.
-        # Z = ackley(X, 20, 0.2, 2 * np.pi, 3)
-        # Z = ackley(X, 20, 0.2, 2 * np.pi, 2)
-        # Y2 = ackley(Y, 20, 0.2, 2 * np.pi, 2)
-        # ax.plot_surface(X, Y, Z)
         a = 20
         b = 0.2
         c = 2 * np.pi
         d = 2
         X,Y = np.meshgrid(x,y)
-        # pprint(X)
-        # Y2 = ackley(X,a,b,c,d)
         Z2 = ackley(X, Y,a,b,c,d)
 
-        # fig = plt.figure()
-        # ax = plt.axes(projection='3d')
-        # ax.contour3D(X, Y, Z, 50, cmap='binary')
         ax.plot_surface(X, Y, Z2, cmap="coolwarm")
         ax.set_xlabel('x')
         ax.set_ylabel('y')
         ax.set_zlabel('z')
 
-        # ax2 = s.fig.add_subplot(projection='3d')
+        # xd = np.random.uniform(s.plane[0],s.plane[1],s.points_cnt)
+        # yd = np.random.uniform(s.plane[0],s.plane[1],s.points_cnt)
+        # def rand_point(x):
+        #     # return x + (0.1 * np.random.randn(100))
+        #     return x + (np.random.choice([-0.1, 0.1], 1) * 100)
+        # dx = rand_point(xd)
+        # dy = rand_point(yd)
+        # # dy = Y # Y  (0.1 * np.random.randn(100))
+        # dz = ackley(dx, dy, a, b, c, d)
+        # s.ax.scatter(dx, dy, dz, marker='o')
+        dx = np.random.choice(x, 32)
+        dy = np.random.choice(y, 32)
+        dz = ackley(dx, dy, a, b, c, d) + 1
 
-        def randrange(n, vmin, vmax):
-            """
-            Helper function to make an array of random numbers having shape (n, )
-            with each number distributed Uniform(vmin, vmax).
-            """
-            return (vmax - vmin) * np.random.rand(n) + vmin
-        n = 100
-        for m, zlow, zhigh in [('o', -50, -25), ('^', -30, -5)]:
-            xs = randrange(n, 23, 32)
-            ys = randrange(n, 0, 100)
-            zs = randrange(n, zlow, zhigh)
-            ax.scatter(xs, ys, zs, marker=m)
+        s.ax.scatter(dx, dy, dz, marker='o')
 
         plt.show()
 
-    def alg(self, G):
+    def alg(s):
+        # xs = list(map(lambda p: p.x, s.data))
+        # ys = list(map(lambda p: p.y, s.data))
+        # zs = list(map(lambda p: p.z, s.data))
+        # s.ax.scatter(xs, ys, zs, marker='o')
+
         pass
         # alg(self, G) should have algorithm-required inputs as parameters
 
