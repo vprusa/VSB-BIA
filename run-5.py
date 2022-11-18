7a35180f8ee760608d893915157437618bb957f9
vprusa <prusa.vojtech@gmail.com>
2022-11-18 15:38:37 +0100

    bkp

--- run-5.py
+++ run-5.py
@@ -30,16 +30,16 @@ class Pt(object):
         s.x = x
         s.y = y
 
-
-
 def ackley(xi, yi, a, b, c, d):
     part1 = - a * np.exp((-b * np.sqrt((1.0 / d) * (np.power(xi, 2) + np.power(yi, 2)))))
     part2 = - np.exp((1.0 / d) * (np.cos(c * xi) + np.cos(c * yi)))
     return part1 + part2 + a + np.exp(1)
-    # return (100 * np.power((xi - np.power(yi, 2)), 2) + np.power((yi - 1), 2))
+    return (100 * np.power((xi - np.power(yi, 2)), 2) + np.power((yi - 1), 2))
     # plane = [-10, 10, 30]
 
     # def alg(s, dx, dy):
+    # dx = xi
+    # dy = yi
     # def single1(x):
     #     return np.power(x,2)
     # def single2(x, i):
@@ -48,16 +48,6 @@ def ackley(xi, yi, a, b, c, d):
     #     return 0.5 * i * x
     # return single1(dx) + single1(dy) + np.power(single2(dx,1) + single2(dy,2),2) + np.power(single3(dx,1) + single3(dy,2),4)
 
-
-def ackley2(xi, a, b, c, d):
-    # part1 = - a * np.exp((-b * np.sqrt((1.0 / d) * (np.power(xi, 2) + np.power(yi, 2)))))
-    # part2 = - np.exp((1.0 / d) * (np.cos(c * xi) + np.cos(c * yi)))
-    # return part1 + part2 + a + np.exp(1)
-    # return (100 * np.power((xi - np.power(yi, 2)), 2) + np.power((yi - 1), 2))
-    # plane = [-10, 10, 30]
-    part1 = - a * np.exp((-b * np.sqrt((1.0 / d) * (np.power(xi, 2)))))
-    part2 = - np.exp((1.0 / d) * (np.cos(c * xi)))
-    return part1 + part2 + a + np.exp(1)
 
     # def alg(s, dx, dy):
     # def single1(x):
@@ -73,7 +63,7 @@ def f(xi, xi1):
     # Rosenbrock Function
     return (100 * np.power((xi1 - np.power(xi, 2)), 2) + np.power((xi - 1), 2))
 
-class Vis2D5(object):
+class Vis3D(object):
     frameNo = 0
 
     frameTimeout = 0.1
@@ -105,21 +95,21 @@ class Vis2D5(object):
     cr = 0.7
 
     def __init__(s):
-        Vis2D5.plt = plt
-        Vis2D5.frameNo = 0
+        Vis3D.plt = plt
+        Vis3D.frameNo = 0
 
         s.d = len(s.plane) - 1
 
-        if Vis2D5.fig is None:
-            Vis2D5.fig = plt.figure("BIA - #4 - 4. Differential evolution")
-            Vis2D5.plt.clf()
+        if Vis3D.fig is None:
+            Vis3D.fig = plt.figure("BIA - #5 - 5. Particle swarm optimization")
+            Vis3D.plt.clf()
 
-        ax = Vis2D5.plt.axes(projection='3d')
-        # ax = Vis2D5.plt.axes()
+        ax = Vis3D.plt.axes(projection='3d')
+        # ax = Vis3D.plt.axes()
         s.ax = ax
 
         s.vis_base()
-        # Vis2D5.plt.pause(3)
+        # Vis3D.plt.pause(3)
         s.update()
 
         # swarm = Generate pop_size random individuals (you can use the class Solution mentioned in Exercise 1)
@@ -131,7 +121,9 @@ class Vis2D5(object):
         # s.gbv = s.gen_vel(s.swarm)
         s.gen_vel(s.swarm)
         s.m = 0
-        s.M_max = 100
+        s.M_max = 70
+        s.updatev(1)
+        Vis3D.plt.pause(2)
 
         # while m < M_max :
         while s.m < s.M_max:
@@ -144,14 +136,13 @@ class Vis2D5(object):
                 s.calc_np(i)
         #     Compare a new position of a particle x to its pBest
         #     if new position of x is better than pBest:
-
                 if s.pos_better(i, idx):
         #         pBest = new position of x
                     i.x = i.nx
                     i.y = i.ny
                     i.z = i.nz
         #         if pBest is better than gBest:
-                    if i.z > s.gb.z:
+                    if i.z < s.gb.z:
                         s.gb = i
         #             gBest = pBest
                 idx = idx + 1
@@ -166,28 +157,10 @@ class Vis2D5(object):
 
         s.update()
         s.g = s.g + 1
-        Vis2D5.plt.pause(5)
+        Vis3D.plt.pause(2)
+
 
         s.plt.clf()
-
-    def peq(s, i1, i2):
-        return i1[0] == i2[0] and i1[1] == i2[1]
-
-    def crossover(s, m, t, dims, cr):
-        # generate a uniform random value for every dimension
-        p = np.random.rand(dims)
-        # generate trial vector by binomial crossover
-        trial = [m[i] if p[i] < cr else t[i] for i in range(dims)]
-        return trial
-
-
-    def mutate(s, rr, F):
-        res = [0,0]
-        # def mutate_i(i, ii):
-        #     return (i[0][ii] – i[1][ii]) * F + i[2][ii]
-        res[0] = (rr[0][0] - rr[1][0]) * F + rr[2][0]
-        res[1] = (rr[0][0] - rr[1][0]) * F + rr[2][0]
-        return res
 
     def alg(s, dx, dy):
         # return ackley2(dx, s.a, s.b, s.c, s.d)
@@ -236,17 +209,17 @@ class Vis2D5(object):
         pass
 
     def update(s):
-        dbg("Iteration", Vis2D5.frameNo)
+        dbg("Iteration", Vis3D.frameNo)
         # Scale plot ax
         s.ax.set_xticks([])
         s.ax.set_yticks([])
-        s.ax.set_title(s.__class__.__name__ + ": Step #{} ".format(Vis2D5.frameNo))
+        s.ax.set_title(s.__class__.__name__ + ": Step #{} ".format(Vis3D.frameNo))
 
         # if(s.max_iterations - 5 < VisualizationBase3D.frameNo):
 
     def sleep(s):
-        Vis2D5.plt.pause(s.frameTimeout)
-        Vis2D5.frameNo += 1
+        Vis3D.plt.pause(s.frameTimeout)
+        Vis3D.frameNo += 1
 
     def updatev(s, idx):
         zoffset = 0
@@ -256,16 +229,10 @@ class Vis2D5(object):
         s.nx = list(map(lambda i: i.nx, s.swarm))
         s.ny = list(map(lambda i: i.ny, s.swarm))
         s.nz = list(map(lambda i: i.nz + zoffset, s.swarm))
-        # s.dvx = list(map(lambda i: i.vx, s.swarm))
-        # s.dvy = list(map(lambda i: i.vy, s.swarm))
-        # s.dvz1 = list(map(lambda i: i.vz, s.swarm))
-        # s.dvz = list(map(lambda i: i.vz + zoffset, s.swarm))
         s.ax.scatter(s.dx, s.dy, s.dz, marker='.', color="red")
         # if(idx > 0):
         i = 0
         for x in s.dx:
-            # s.ax.plot([s.dx[i],s.dx[i]+s.dvx[i]], [s.dy[i],s.dy[i]+s.dvy[i]], zs=[s.dz[i],s.dz[i]+s.dvz[i] + zoffset])
-            # s.ax.plot([s.dx[i], s.dx[i]+s.dvx[i]], [s.dy[i], s.dy[i]+s.dvy[i]], zs=[s.dz[i], s.dz[i] + s.dvz1[i]])
             if s.nz[i] != 0:
                 s.ax.plot([s.dx[i], s.nx[i]], [s.dy[i], s.ny[i]], zs=[s.dz[i], s.nz[i]])
             i = i + 1
@@ -274,11 +241,11 @@ class Vis2D5(object):
     # wc = 0.5
     # phi_p = 0.5
     # phi_g = 0.5
-    wc = 0.9
-    phi_p = 0.5
-    phi_g = 0.9
-    vmax_len = 5
-    v_len = 2
+    wc = 0.1
+    phi_p = 0.1
+    phi_g = 0.1
+    vmax_len = 3
+    v_len = 3
 
     def calc_vel(s, i):
         def trim(x):
@@ -288,15 +255,15 @@ class Vis2D5(object):
                 x = -s.vmax_len
             return x
         def ru():
-            return random.uniform(0,1)
-        def cv(x, gx):
+            return random.uniform(0, 1)
+        def cv(v, gx, pi, xi):
             # random.choice([-1.0, 1.0])
-            return (x * s.wc + ru() * s.phi_p * (x) + s.phi_g * ru() * (gx))
+            return -(v * s.wc + ru() * s.phi_p * (pi - xi) + s.phi_g * ru() * (gx - xi))
 
-        i.vx = cv(i.vx, s.gb.vx)
+        i.vx = cv(i.vx, s.gb.x, i.x, s.gb.x)
         i.vx = trim(i.vx)
 
-        i.vy = cv(i.vy, s.gb.vy)
+        i.vy = cv(i.vy, s.gb.y, i.y, s.gb.y)
         i.vy = trim(i.vy)
 
         # i.vz = s.alg(i.vx, i.vy)
@@ -322,7 +289,7 @@ class Vis2D5(object):
     def pos_better(s, i, idx):
         i.z = s.alg(i.x, i.y)
         i.nz = s.alg(i.nx, i.ny)
-        return i.nz > i.z
+        return i.nz < i.z
 
     def calc_np(s, i):
         nx = i.x + i.vx * s.v_len
@@ -358,6 +325,110 @@ class Vis2D5(object):
         return bidx
 
 
-r = Vis2D5()
+
+
+
+class Sphere(Vis3D):
+
+    def alg(s, dx, dy):
+        return (np.power(dx, 2) + (np.power(dy, 2)))
+
+class Schwefel(Vis3D):
+    """
+    Problematic, neads tweaking of params for generating next generation
+    """
+    plane = [-500, 500, 500]
+
+    c = 418.9829
+
+    def alg(s, dx, dy):
+        return (s.c * s.d - ( (dx * np.sin(np.sqrt(np.abs(dx))))) + (dy * np.sin(np.sqrt(np.abs(dy)))))
+
+class Rosenbrock(Vis3D):
+    def alg(s, dx, dy):
+        return (100 * np.power((dy - np.power(dx, 2)), 2) + np.power((dx - 1), 2))
+
+class Rastrigin(Vis3D):
+
+    plane = [-5, 5, 30]
+
+    c = 10
+
+    def alg(s, dx, dy):
+        def single(x):
+            return (np.power(x, 2) - s.c * np.cos(2 * np.pi * x))
+        return ((s.c * s.d ) + single(dx) + single(dy))
+
+class Griewangk(Vis3D):
+    # https://en.wikipedia.org/wiki/Griewank_function
+
+    plane = [-100, 100, 80]
+
+    def alg(s, dx, dy):
+        def single2(x, i):
+            return np.cos(x/np.sqrt(i))
+        def single1(x, i):
+            return np.power(x,2)
+        return 1 + 1/4000 * (single1(dx, 1) + single1(dy, 2)) - (single2(dx, 1) * single2(dy, 2))
+
+class Levy(Vis3D):
+
+    plane = [-10, 10, 30]
+
+    def alg(s, dx, dy):
+        def wi(x):
+            return 1 + (x -1)/4
+        def single(x):
+            return np.power((wi(x) -1),2) * (1+10*np.power(np.sin(np.pi * wi(x) + 1), 2))
+        return np.power(np.sin(np.pi * wi(dx)),2) + single(dx) + single(dy) + np.power(wi(dy)-1, 2)*(1+(np.power(np.sin(2*np.pi*wi(dy)),2)))
+
+class Michalewicz(Vis3D):
+
+    m = 10
+    plane = [0, 4, 30]
+
+    def alg(s, dx, dy):
+        def single(x, i):
+            return (np.sin(x)*np.power(np.sin((i*np.power(x,2))/np.pi),2*s.m))
+        return - (single(dx, 1) + single(dy,2))
+
+class Zakharov(Vis3D):
+
+    plane = [-10, 10, 30]
+
+    def alg(s, dx, dy):
+        def single1(x):
+            return np.power(x,2)
+        def single2(x, i):
+            return 0.5 * i * x
+        def single3(x, i):
+            return 0.5 * i * x
+        return single1(dx) + single1(dy) + np.power(single2(dx,1) + single2(dy,2),2) + np.power(single3(dx,1) + single3(dy,2),4)
+
+class Ackley(Vis3D):
+    a = 20
+    b = 0.2
+    c = 2 * np.pi
+    d = 2
+
+    def alg(s, xi, yi):
+        part1 = - s.a * np.exp((-s.b * np.sqrt((1.0 / s.d) * (np.power(xi, 2) + np.power(yi, 2)))))
+        part2 = - np.exp((1.0 / s.d) * (np.cos(s.c * xi) + np.cos(s.c * yi)))
+        return part1 + part2 + s.a + np.exp(1)
+
+
+# while True:
+# Sphere, Schwefel, Rosenbrock, Rastrigin, Griewangk, Levy, Michalewicz, Zakharov, Ackley
+
+r = Sphere()
+r = Schwefel()
+r = Rosenbrock()
+r = Rastrigin()
+r = Griewangk()
+r = Levy()
+r = Michalewicz()
+r = Zakharov()
+r = Ackley()
+# r = Vis3D()
 
 exit(0)
